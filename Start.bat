207683127136a8fc37af54ab7a6cb38e929a93b2
Alrbuddy <93697854+Alrbuddy@users.noreply.github.com>
2023-06-20 13:21:10 -0500

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,1 +1,33 @@
+@echo off
 
+set "scriptpath=%~dp0"
+set "supplementarypath=%scriptpath%\supplementary_commands"
+
+:DELETE_CONFIRMATION
+if exist "%scriptpath%\confirmation.txt" (
+    del "%scriptpath%\confirmation.txt"
+    goto START_SCRIPT
+)
+
+:START_SCRIPT
+if exist "%supplementarypath%\STATUS.txt" (
+    findstr /C:"RUN_NPM_UPDATE" "%supplementarypath%\STATUS.txt" >nul
+    if not errorlevel 1 (
+        echo "Adolf's Spoofer - Updating required NPM packages"
+        cd /d "%supplementarypath%" && call npm i
+    )
+)
+
+title Adolfs Spoofer
+
+start /B python "%scriptpath%\main.py"
+
+timeout /t 5 > nul
+
+:WAIT_CONFIRMATION
+if not exist "%scriptpath%\confirmation.txt" (
+    timeout /t 5 > nul
+    goto WAIT_CONFIRMATION
+)
+
+start /B node "%scriptpath%\Default.mjs"
